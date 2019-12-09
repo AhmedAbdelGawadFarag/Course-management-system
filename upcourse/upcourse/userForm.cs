e0ea13f3752e03acb5fb27dcbf5001852c5ee05b
Shehab-Ahmed-Bassuone﻿ <Shehab.Ahmed.Bassuone@gmail.com>
2019-12-09 21:34:28 +0200

    update design

---
+++ upcourse/upcourse/userForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upcourse
+{
+    public partial class userForm : Form
+    {
+        public userForm()
+        {
+            InitializeComponent();
+        }
+
+        private void userForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel11_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
