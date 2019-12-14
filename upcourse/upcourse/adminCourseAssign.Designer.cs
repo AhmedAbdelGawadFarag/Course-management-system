2358c7d23de3878afc8ecd20894fa1fa114d7907
ahmed503as <ahmed503.as@gmail.com>
2019-12-15 01:32:07 +0200

    assign trainer to courses

---
+++ upcourse/upcourse/adminCourseAssign.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace upcourse
+{
+    partial class adminCourseAssign
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trainerCombo = new System.Windows.Forms.ComboBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.assignButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trainerCombo);
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Controls.Add(this.assignButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 152);
+            this.panel1.TabIndex = 0;
+            // 
+            // trainerCombo
+            // 
+            this.trainerCombo.FormattingEnabled = true;
+            this.trainerCombo.Location = new System.Drawing.Point(40, 51);
+            this.trainerCombo.Name = "trainerCombo";
+            this.trainerCombo.Size = new System.Drawing.Size(184, 21);
+            this.trainerCombo.TabIndex = 2;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(142, 96);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(82, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // assignButton
+            // 
+            this.assignButton.Location = new System.Drawing.Point(40, 96);
+            this.assignButton.Name = "assignButton";
+            this.assignButton.Size = new System.Drawing.Size(80, 23);
+            this.assignButton.TabIndex = 0;
+            this.assignButton.Text = "Assign";
+            this.assignButton.UseVisualStyleBackColor = true;
+            this.assignButton.Click += new System.EventHandler(this.assignButton_Click);
+            // 
+            // adminCourseAssign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "adminCourseAssign";
+            this.Size = new System.Drawing.Size(262, 152);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox trainerCombo;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button assignButton;
+    }
+}
---
+++ upcourse/upcourse/adminCourseAssign.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace upcourse
+{
+    public partial class adminCourseAssign : UserControl
+    {
+        public int courseID;
+        public adminCourseAssign()
+        {
+            InitializeComponent();
+            updateCombo();
+        }
+
+        public void setCourseId(int id)
+        {
+            this.courseID = id;
+        }
+        public void updateCombo()
+        {
+            this.trainerCombo.Items.Clear();
+            SqlCommand cmd = new SqlCommand("select ID,firstName,LastName from trainer", Program.dbconnection);
+            SqlDataReader data = cmd.ExecuteReader();
+            while(data.Read())
+            {
+                this.trainerCombo.Items.Add(data.GetString(data.GetOrdinal("firstName")) + " " + data.GetString(data.GetOrdinal("LastName")) + " -" + data.GetInt32(data.GetOrdinal("ID")) );
+            }
+            data.Close();
+
+        }
+        private void assignButton_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into CourseTrainer values(@TrainerID,@CourseID)",Program.dbconnection);
+            cmd.Parameters.AddWithValue("CourseID", this.courseID);
+            string cid = this.trainerCombo.SelectedItem.ToString().Substring (this.trainerCombo.SelectedItem.ToString().IndexOf('-') + 1);
+            cmd.Parameters.AddWithValue("TrainerID", cid);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
